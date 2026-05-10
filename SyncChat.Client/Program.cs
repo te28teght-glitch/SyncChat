16ebdb1d98fddb98305b3b8ccbe54eeb58b660e0
te28teght-glitch <te28teght@gmail.com>
2026-05-10 15:37:22 +0300

    гит игнор

--- SyncChat.Client/Program.cs
+++ SyncChat.Client/Program.cs
@@ -15,15 +15,20 @@ namespace SyncChat.Client
             Console.WriteLine("Подключено к серверу!");
             NetworkStream stream = client.GetStream();
             Console.Write("Введите сообщение для сервера: ");
+
             string message = Console.ReadLine();
             byte[] data = Encoding.UTF8.GetBytes(message);
+
             stream.Write(data, 0, data.Length);
             Console.WriteLine("Сообщение отправлено");
+
             byte[] buffer = new byte[1024];
             int bytesRead = stream.Read(buffer, 0, buffer.Length);
             string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
             Console.WriteLine($"Прочитано байт: {bytesRead}");
             Console.WriteLine($"Ответ от сервера: {response}");
+            
             client.Close();
             Console.WriteLine("Нажмите любую кнопку для выхода...");
             Console.ReadKey();
