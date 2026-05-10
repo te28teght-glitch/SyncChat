b3a16cc60adead23d94f09b5dc3fa62ec249b22d
te28teght-glitch <Artemtwoartem@mail.ru>
2026-05-10 15:29:33 +0300

    Гитигнор

---
+++ SyncChat.Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+
+namespace SyncChat.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 8888;
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1",port);
+            Console.WriteLine("Подключено к серверу!");
+            NetworkStream stream = client.GetStream();
+            Console.Write("Введите сообщение для сервера: ");
+            string message = Console.ReadLine();
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine("Сообщение отправлено");
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Прочитано байт: {bytesRead}");
+            Console.WriteLine($"Ответ от сервера: {response}");
+            client.Close();
+            Console.WriteLine("Нажмите любую кнопку для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SyncChat.Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SyncChat.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 8888);
+            listener.Start();
+            Console.WriteLine("Сервер запущен. Порт:8888. Ожидание подключения...");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Клиент подключился!");
+
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Получено: {receivedMessage}");
+
+                byte[] response = Encoding.UTF8.GetBytes(receivedMessage);
+                stream.Write(response, 0, response.Length);
+                Console.WriteLine("Ответ отправлен клиенту");
+    
+                
+                client.Close();
+            }
+        }
+    }
+}
